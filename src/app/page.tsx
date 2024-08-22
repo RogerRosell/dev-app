8c305e4916f69554435a9506c91e6a560eaf02a6
RogerRosell <r@roger-rossell.com>
2024-08-22 23:33:13 +0100

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <div>Hello :)</div>
       <div>
         <Link href="./brands">Brands</Link>
+        </div>
+      <div>
         <Link href="./developers">Developers</Link>
       </div>
     </main>
