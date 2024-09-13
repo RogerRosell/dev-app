f903a24f7f80e2b0673fb6ba2697c484e6d7d1d6
RogerRosell <r@roger-rossell.com>
2024-09-13 20:14:21 +0100

    Removed timer

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -1,19 +1,20 @@
 "use client"
 
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+// import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 
 const Timer = () => {
   return (
     <div>
-    <CountdownCircleTimer
+      Timer
+    {/* <CountdownCircleTimer
     isPlaying
     duration={60}
     colors={['#004777', '#F7B801', '#A30000', '#A30000']}
     colorsTime={[7, 5, 2, 0]}
   >
     {({ remainingTime }) => remainingTime}
-  </CountdownCircleTimer>
+  </CountdownCircleTimer> */}
   </div>
   )
 }
