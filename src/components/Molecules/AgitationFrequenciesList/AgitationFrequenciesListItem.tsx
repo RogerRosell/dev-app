376f23385cbe92312956004402347fbb530155ab
RogerRosell <r@roger-rossell.com>
2024-09-28 01:58:19 +0100

    fix

--- src/components/Molecules/AgitationFrequenciesList/AgitationFrequenciesListItem.tsx
+++ src/components/Molecules/AgitationFrequenciesList/AgitationFrequenciesListItem.tsx
@@ -1,7 +1,7 @@
-import { useAgitationFrequenciesStore } from '@/stores/agitation-frequencies-store';
+import { useAgitationFrequencyStore } from '@/stores/agitation-frequencies-store';
 
 export const AgitationFrequenciesListItem = ({id, frequency}: any) => {
-  const { deleteAgitationFrequency } = useAgitationFrequenciesStore(); 
+  const { deleteAgitationFrequency } = useAgitationFrequencyStore(); 
   return (
     <li key={id}><a onClick={() => deleteAgitationFrequency(id)} className="underline cursor-pointer">X</a>{" "} {frequency}</li>
   )
